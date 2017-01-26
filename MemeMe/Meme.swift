80031b7391d19b8dee3fb334a0e3a358d47b8b8a
anirudh.r <anirudh.r@flipkart.com>
2017-01-27 00:41:42 +0530

    created meme structure

---
+++ MemeMe/Meme.swift
@@ -0,0 +1,18 @@
+//
+//  Meme.swift
+//  MemeMe
+//
+//  Created by Anirudh Ramanan on 27/01/17.
+//  Copyright © 2017 Anirudh Ramanan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    var topText:String
+    var bottomText:String
+    var image:UIImage
+    var memeImage:UIImage
+    
+}
---
+++ MemeMe/MemeTextFieldDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  MemeTextFieldDelegate.swift
+//  MemeMe
+//
+//  Created by Anirudh Ramanan on 26/01/17.
+//  Copyright © 2017 Anirudh Ramanan. All rights reserved.
+//
+
+import Foundation
