19a8fa9f5149fe9f0e190433819fb8b032cb237e
anirudh.r <anirudh.r@flipkart.com>
2017-01-28 21:35:44 +0530

    save the meme before dismissing the view controller

--- MemeMe/MemeViewController.swift
+++ MemeMe/MemeViewController.swift
@@ -12,25 +12,21 @@ class MemeViewController: UIViewController, UIImagePickerControllerDelegate, UIN
     
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var cameraButton: UIBarButtonItem!
-    
     @IBOutlet weak var topText: UITextField!
     @IBOutlet weak var bottomText: UITextField!
     @IBOutlet weak var shareButton: UIBarButtonItem!
     
     let memeTextFieldDelegate = MemeTextFieldDelegate()
-    
+    var memeImage: UIImage!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        memeTextFieldDelegate.view = self.view
-        self.topText.delegate = memeTextFieldDelegate
-        self.bottomText.delegate = memeTextFieldDelegate
-        self.shareButton.isEnabled = false
+        setupTextField()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
         self.memeTextFieldDelegate.subscribeToKeyboardNotifications()
-        self.tabBarController?.tabBar.isHidden = true
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -46,36 +42,49 @@ class MemeViewController: UIViewController, UIImagePickerControllerDelegate, UIN
         openImagePickerController(sender)
     }
     
+    @IBAction func chooseFont(_ sender: Any) {
+        let fontController = UIAlertController(title: "Choose Font", message: "Select your favourite font", preferredStyle: .actionSheet)
+        for fontName in getFontsList() {
+            fontController.addAction(UIAlertAction(title: fontName, style: .default, handler: updateSelectedFont))
+        }
+        present(fontController, animated: true, completion: nil)
+    }
+    
+    private func updateSelectedFont(action: UIAlertAction){
+        let fontName = action.title ?? ""
+        //make sure that the font has a title, else do not execute this block
+        if fontName != "" {
+            self.topText.defaultTextAttributes = getMemeTextAttributes(fontName: fontName)
+            self.bottomText.defaultTextAttributes = getMemeTextAttributes(fontName: fontName)
+        }
+    }
+    
+    private func setupTextField() {
+        //first set the delegates for both text fields
+        memeTextFieldDelegate.view = self.view
+        self.topText.delegate = memeTextFieldDelegate
+        self.bottomText.delegate = memeTextFieldDelegate
+        
+        //sent fonts
+        self.topText.defaultTextAttributes = getMemeTextAttributes(fontName: "HelveticaNeue-CondensedBlack")
+        self.bottomText.defaultTextAttributes = getMemeTextAttributes(fontName: "HelveticaNeue-CondensedBlack")
+    }
+    
     @IBAction func cancel(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
     
-    func openImagePickerController(_ sender : Any) {
-        let imagePicker = UIImagePickerController()
-        imagePicker.delegate = self
-        
-        switch (sender as AnyObject).tag {
-        case 0:
-            imagePicker.sourceType = .camera
-            break
-        case 1:
-            imagePicker.sourceType = .photoLibrary
-            break
-        default:
-            fatalError()
-            break
-        }
-        
-        self.present(imagePicker, animated: true, completion: nil)
-    }
-    
     @IBAction func shareMemeImage(_ sender: Any) {
-        let shareController = UIActivityViewController(activityItems: [getMemeImage()], applicationActivities: nil)
-        shareController.completionWithItemsHandler = {
-            (s, ok, items, error) in print(ok ? "SUCCESS!" : "FAILURE")
-            self.save()
-        }
+        memeImage = getMemeImage()
+        let shareController = UIActivityViewController(activityItems: [memeImage], applicationActivities: nil)
         self.present(shareController, animated: true, completion: nil)
+        shareController.completionWithItemsHandler = {
+            (a, success, i, e) in ()
+            if success {
+                self.save(memeImage: self.memeImage)
+                self.dismiss(animated: true, completion: nil)
+            }
+        }
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
@@ -85,39 +94,10 @@ class MemeViewController: UIViewController, UIImagePickerControllerDelegate, UIN
         imageView.image = image
         imageView.contentMode = .scaleAspectFit
         dismiss(animated: true, completion: nil)
-        checkEnableShareButton()
+        self.shareButton.isEnabled = true
     }
     
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         dismiss(animated: true, completion: nil)
-    }
-    
-    func save() {
-        // Create the meme
-        let meme = Meme(topText: topText.text ?? "", bottomText: bottomText.text ?? "", image: imageView.image!, memeImage: getMemeImage())
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        appDelegate.memes.append(meme)
-        performSegue(withIdentifier: "unwindToList", sender: self)
-    }
-    
-    func getMemeImage() -> UIImage {
-        self.navigationController?.toolbar.isHidden = true
-        self.navigationController?.navigationBar.isHidden = true
-
-        UIGraphicsBeginImageContext(self.view.frame.size)
-        view.drawHierarchy(in: self.view.frame, afterScreenUpdates: true)
-        let memeImage: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
-        UIGraphicsEndImageContext()
-
-        self.navigationController?.toolbar.isHidden = false
-        self.navigationController?.navigationBar.isHidden = false
-
-        return memeImage
-    }
-    
-    func checkEnableShareButton() {
-        if imageView.image != nil {
-            self.shareButton.isEnabled = true
-        }
     }
 }
