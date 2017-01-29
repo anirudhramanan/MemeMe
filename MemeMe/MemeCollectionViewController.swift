30588b2526acbfb3c60bebfc67622f5db37de4f2
anirudh.r <anirudh.r@flipkart.com>
2017-01-29 20:56:10 +0530

    meme detial view implemented for collectionview

--- MemeMe/MemeCollectionViewController.swift
+++ MemeMe/MemeCollectionViewController.swift
@@ -57,4 +57,26 @@ class MemeCollectionViewController: UICollectionViewController {
         sentMemes = SentMemes.allSentMemes()
         collectionView?.reloadData()
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        
+        switch(segue.identifier ?? ""){
+        case "ShowMeme":
+            guard let memeDetailViewController = segue.destination as? MemeDetailViewController else{
+                fatalError("Unexpected destination: \(segue.destination)")
+            }
+            guard let selectedMemeCell = sender as? MemeCollectionViewCell else {
+                fatalError("Unexpected sender: \(sender)")
+            }
+            guard let indexPath = collectionView?.indexPath(for: selectedMemeCell) else {
+                fatalError("The selected cell is not being displayed by the table")
+            }
+            let selectedMeme = sentMemes[indexPath.row]
+            memeDetailViewController.meme = selectedMeme
+            break
+        default:
+            break
+        }
+    }
 }
